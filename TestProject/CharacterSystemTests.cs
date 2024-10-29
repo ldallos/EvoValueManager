22dd2f88eccbd8ff8a9719fd77f4a2c577587720
z003pw3j <laszlo.dallos.ext@siemens-healthineers.com>
2024-10-29 22:41:31 +0100

    Manual system test case sample.

---
+++ TestProject/CharacterSystemTests.cs
@@ -0,0 +1,37 @@
+﻿namespace TestProject
+{
+    internal class CharacterSystemTests
+    {
+        /// <summary>
+        /// EVMVAL001
+        /// EVMCHAR002
+        /// </summary>
+        public void AddCharacterTest()
+        {
+            // Step 01: Elindítom az Evo Értékeink oldalt.
+            // Expr 01: Látom az Evo Értékeink oldal Főoldalát.
+
+            // Step 02: Rákattintok a Csapattagok tabra.
+            // Expr 02: Látom a Csapat kezelő oldalt a csapattag választó dropdownnal és a + csapattag hozzáadás gombot.
+
+            // Step 03: Rákattintok a + csapattag hozzáadás gombra.
+            // Expr 03: Látom a csapattag felvételéhez szükséges beviteli mezőket:
+            //          Név, Fejlődés, Gondoskodás, Jelenlét, Megbízhatóság, Merészség. Valamint egy Mentés gombot.
+
+            // Step 04: Kitöltöm a mezőket a következő értékekkel, majd megnyomom a Mentés gombot:
+            //          Név: Teszt Csapattag
+            //          Fejlődés: 1
+            //          Gondoskodás: 2
+            //          Jelenlét: 3
+            //          Megbízhatóság: 4 
+            //          Merészség: 5
+            // Expr 04: Látom a Csapat kezelő oldalt a csapattag választó dropdownnal és a + csapattag hozzáadás gombot.
+
+            // Step 05: Rákattintok a csapattag választó dropdownra.
+            // Expr 05: A legördülő listában ott látom Teszt Csapattag nevét.
+
+            // Step 06: ???
+            // Expr 06: ???
+        }
+    }
+}
