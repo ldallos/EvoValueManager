a2add72c0b3a33b6c0239f223a093de12559a867
dombi-balazs <dombi.balazs.sandor@gmail.com>
2025-05-15 18:57:07 +0200

    removed duplicated files and formatted the codes

--- EvoValueManager.Client/src/i18n/index.ts
+++ EvoValueManager.Client/src/i18n/index.ts
@@ -6,19 +6,18 @@ import en from "./locales/en/translation.json";
 import hu from "./locales/hu/translation.json";
 
 const resources = {
-  en: { translation: en },
-  hu: { translation: hu },
+    en: { translation: en },
+    hu: { translation: hu },
 } as const;
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    fallbackLng: "en",
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: "en",
+        interpolation: {
+            escapeValue: false,
+        },
+    });
 
 export default i18n;
--- EvoValueManager.Client/src/pages/HomePage.tsx
+++ EvoValueManager.Client/src/pages/HomePage.tsx
@@ -2,34 +2,38 @@
 import { useTranslation } from "react-i18next";
 
 function HomePage() {
-  const { t } = useTranslation();
-  return (
-    <div className="home-page container">
-      <div className="text-center">
-        <h1 className="display-4">EvoValueManager</h1>
-        <div className="values-intro">
-          <p>{t("welcome")}</p>
-          <p>{t("setEvoValue")}</p>
-          <p>{t("homeDescription")}</p>
-        </div>
-      </div>
+    const { t } = useTranslation();
+    return (
+        <div className="home-page container">
+            <div className="text-center">
+                <h1 className="display-4">EvoValueManager</h1>
+                <div className="values-intro">
+                    <p>{t("welcome")}</p>
+                    <p>{t("setEvoValue")}</p>
+                    <p>{t("homeDescription")}</p>
+                </div>
+            </div>
 
-      {TRAITS.map((trait, index) => (
-        <div
-          key={trait.property}
-          className={`values-container ${index % 2 !== 0 ? "mirror" : ""}`}
-        >
-          <div className="text-area">
-            <h2>{trait.title}</h2>
-            <p>{trait.description}</p>
-          </div>
-          <div className="image-area">
-            <img src={trait.image} alt={trait.title} loading="lazy" />
-          </div>
+            {TRAITS.map((trait, index) => (
+                <div
+                    key={trait.property}
+                    className={`values-container ${index % 2 !== 0 ? "mirror" : ""}`}
+                >
+                    <div className="text-area">
+                        <h2>{trait.title}</h2>
+                        <p>{trait.description}</p>
+                    </div>
+                    <div className="image-area">
+                        <img
+                            src={trait.image}
+                            alt={trait.title}
+                            loading="lazy"
+                        />
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default HomePage;
